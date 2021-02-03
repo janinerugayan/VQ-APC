334aba749fb8ceadaaccfe7aab8761a670b484ce
Janine Rugayan <janinelr@stud.ntnu.no>
2021-02-03 19:10:08 +0100

    model.saveVQparam

--- train_vqapc.py
+++ train_vqapc.py
@@ -223,7 +223,7 @@ def main():
       open(os.path.join(model_dir, config.exp_name + '__epoch_%d' %
       (epoch_i + 1) + '.model'), 'wb'))
 
-    saveVQparam(model_dir, config.exp_name, epoch_i)
+    model.saveVQparam(model_dir, config.exp_name, epoch_i)
 
 
 if __name__ == '__main__':
