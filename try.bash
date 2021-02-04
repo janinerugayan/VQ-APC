f9d09b187bda08f010fb4eec5a8e291cde8f9cf8
Janine Rugayan <janinelr@stud.ntnu.no>
2021-02-04 13:36:17 +0100

    changed path

--- try.bash
+++ try.bash
@@ -1,4 +1,4 @@
 CUDA_VISIBLE_DEVICES=1 python VQ-APC_downstream_try.py --exp_name=combined_sounds_shuffled \
 --sound_file=./wavs/combined_sounds_shuffled.wav \
 --pretrained_weights=./logs/feb-3_vqextract_trial1.dir/feb-3_vqextract_trial1__epoch_1.model \
---pretrained_VQ=./logs/feb-4_vqextract_trial1.dir/feb-4_vqextract_trial1VQ-layers__epoch_1.model 
+--pretrained_VQ=./logs/feb-4_vqextract.dir/feb-4_vqextractVQ-layers__epoch_1.model 
