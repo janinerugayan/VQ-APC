cc974c5fbb369c376a541a907cbe262391cf038b
Janine Rugayan <janinelr@stud.ntnu.no>
2021-02-04 10:21:56 +0100

    added .module in the saveVQparam method of class

--- vqapc_model.py
+++ vqapc_model.py
@@ -158,7 +158,7 @@ class GumbelAPCModel(nn.Module):
 
   # for saving VQ Layer parameters
   def saveVQparam(self, model_dir, exp_name, epoch_i):
-    torch.save(self.vq_layers.state_dict(),
+    torch.save(self.vq_layers.module.state_dict(),
       open(os.path.join(model_dir, exp_name + 'VQ-layers__epoch_%d' %
       (epoch_i + 1) + '.model'), 'wb'))
 
